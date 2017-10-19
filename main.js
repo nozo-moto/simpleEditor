4ffe40c46fa85ecd085b91c35acac08d3f85e7ed
Nozomi Morimoto <nozomotoitech@gmail.com>
2017-10-20 00:42:00 +0900

    サイドバーを指定箇所に

--- main.js
+++ main.js
@@ -4,9 +4,12 @@ const BrowserWindow = electron.BrowserWindow;
   
 const path = require('path');
 const url = require('url');
+const fs = require('fs');
 
 let mainWindow;
-  
+
+fs.accessSync('./textfile');
+
 function createWindow () {
   mainWindow = new BrowserWindow({width: 800, height: 600});
   
