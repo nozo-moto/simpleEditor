4b783b633a149448553f3ccf86e224ba91a57733
Your Name <nozomotoitech@gmail.com>
2017-10-20 14:43:04 +0900

    修正

--- index.js
+++ index.js
@@ -2,17 +2,33 @@ const editor = ace.edit("rightside");
 const fs=require("fs");
 
 editor.setTheme("ace/theme/github");
-editor.getSession().setMode("ace/mode/text");
+editor.getSession().setMode("ace/mode/ayu-light");
 editor.setShowPrintMargin(false);
-
-$("#rightside").on("keydown", function (b) {
+editor.renderer.setShowGutter(false);
+$("#rightside").on("keydown", function () {
     autoSave(saveFile);
+});
+
+$("#leftside > #memobars > #memobar").on("click", function(e){
+    console.log($(this).attr("id"));
 });
 
 function autoSave(callback) {
     callback();
 }
+
 const saveFile = function () {
     fs.writeFileSync("sample.txt", editor.getValue());
     console.log('saved');
+}
+
+const readFile = function(filepath){
+    const path = "./textfile/" + filepath
+    fs.readFile(path, function (error, text) {
+      if (error != null) {
+        alert('error : ' + error);
+        return;
+      }
+      editor.setValue(text.toString(), -1);
+    });  
 }
--- leftsidebar.js
+++ leftsidebar.js
@@ -2,7 +2,7 @@ const leftside = document.getElementById('memobars');
 fs.readdir('./textfile', function (err, files) {
     if (err) throw err;
     files.forEach(function (e) {
-        const bar = document.createElement('li');
+        const bar = document.createElement('div');
         bar.setAttribute("id", "memobar");
         const title = document.createElement('text');
         title.appendChild(document.createTextNode(e));
